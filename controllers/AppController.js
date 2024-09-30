01d2ad59fc1433329748f382f0a213097369d524
AerinLom <aerinlom1@gmail.com>
2024-09-30 12:47:49 -0700

    Task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import { dbClient } from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+
+class AppController {
+    static async getStatus(req, res) {
+        const redisAlive = redisClient.isAlive();
+        const dbAlive = dbClient.isAlive();
+
+        return res.status(200).json({ redis: redisAlive, db: dbAlive });
+    }
+
+    static async getStats(req, res) {
+        const usersCount = await dbClient.nbUsers();
+        const filesCount = await dbClient.nbFiles();
+
+        return res.status(200).json({ users: usersCount, files: filesCount });
+    }
+}
+
+export default AppController;
