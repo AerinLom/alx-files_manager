3f885e504b054582856ebc66c11e240a92f15f5f
AerinLom <aerinlom1@gmail.com>
2024-10-01 05:50:43 -0700

    Made changes to task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -10,31 +10,22 @@ export default class UsersController {
     const password = req.body ? req.body.password : null;
 
     if (!email) {
-      console.log('Missing email in request');
       res.status(400).json({ error: 'Missing email' });
       return;
     }
     if (!password) {
-      console.log('Missing password in request');
       res.status(400).json({ error: 'Missing password' });
       return;
     }
-
     const user = await (await dbClient.usersCollection()).findOne({ email });
 
     if (user) {
-      console.log('Email already exists:', email);
       res.status(400).json({ error: 'Already exist' });
       return;
     }
-
-    const hashedPassword = sha1(password);
-    const usersCollection = await dbClient.usersCollection();
-    const insertionInfo = await usersCollection.insertOne({ email, password: hashedPassword });
-
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
     const userId = insertionInfo.insertedId.toString();
-
-    console.log(`User created with ID: ${userId}`);
 
     userQueue.add({ userId });
     res.status(201).json({ email, id: userId });
@@ -42,7 +33,7 @@ export default class UsersController {
 
   static async getMe(req, res) {
     const { user } = req;
-    console.log(`Fetching details for user ID: ${user._id}`);
+
     res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
