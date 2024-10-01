13f5f05d408fdea7b939425dc90cf7a526aec565
AerinLom <aerinlom1@gmail.com>
2024-10-01 04:40:15 -0700

    Fixed eslint errors

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,20 @@
-import { dbClient } from '../utils/db.js';
-import redisClient from '../utils/redis.js';
+import { dbClient } from '../utils/db';
+import { redisClient } from '../utils/redis';
 
 class AppController {
-    static async getStatus(req, res) {
-        const redisAlive = redisClient.isAlive();
-        const dbAlive = dbClient.isAlive();
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
 
-        return res.status(200).json({ redis: redisAlive, db: dbAlive });
-    }
+    return res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
 
-    static async getStats(req, res) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
 
-        return res.status(200).json({ users: usersCount, files: filesCount });
-    }
+    return res.status(200).json({ users: usersCount, files: filesCount });
+  }
 }
 
 export default AppController;
