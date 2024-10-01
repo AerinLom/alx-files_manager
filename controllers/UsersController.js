8ef5daf90a186ed3584d7437f09db7eed0be3569
AerinLom <aerinlom1@gmail.com>
2024-10-01 04:55:54 -0700

    Task 3: Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,43 @@
+import { dbClient } from '../utils/db';
+import sha1 from 'sha1';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const existingUser = await dbClient.collection('users').findOne({ email });
+
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPassword = sha1(password);
+
+      const newUser = {
+        email,
+        password: hashedPassword,
+      };
+
+      const result = await dbClient.collection('users').insertOne(newUser);
+
+      return res.status(201).json({
+        id: result.insertedId,
+        email: newUser.email,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server error' });
+    }
+  }
+}
+
+export default UsersController;
